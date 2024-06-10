6269c169a4099c7465b3b6cab7fbefd1f5ca50b2
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-06-10 19:13:39 -0300

    rm remote route for removing log files

--- src/server/api/routers/ssh.ts
+++ src/server/api/routers/ssh.ts
@@ -91,4 +91,32 @@ export const sshRouter = createTRPCRouter({
 
       return stdout;
     }),
+  rm: protectedProcedure
+    .input(z.object({ path: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const name = ctx.session.user.name;
+      if (!name) {
+        throw new Error("No user found");
+      }
+      const keys = getSSHKeys(name);
+      if (!keys) {
+        throw new Error("No keys found for user");
+      }
+
+      const connection = await ssh.connect({
+        host: env.SSH_HOST,
+        username: name,
+        privateKey: keys.privateKey,
+        passphrase: env.SSH_PASSPHRASE,
+      });
+
+      const command = `rm ${input.path}`;
+      const { stdout, stderr } = await connection.execCommand(command);
+
+      if (stderr) {
+        throw new Error(stderr);
+      }
+
+      return stdout;
+    }),
 });
