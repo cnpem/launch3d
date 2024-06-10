023b7962a2b68c8453df3feb414732a653a4a508
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-06-10 19:14:52 -0300

    simplify log view component states

--- src/app/instances/logs.tsx
+++ src/app/instances/logs.tsx
@@ -9,11 +9,7 @@ import {
 import { Textarea } from "~/app/_components/ui/textarea";
 import { jobName } from "~/lib/constants";
 
-export default function LogView({
-  jobId,
-}: {
-  jobId: string;
-}) {
+export default function LogView({ jobId }: { jobId: string }) {
   const stdout = api.ssh.cat.useQuery({
     path: `~/${jobName}-${jobId}.out`,
   });
@@ -22,39 +18,28 @@ export default function LogView({
     path: `~/${jobName}-${jobId}.err`,
   });
 
-  function checkData(data: string | undefined, isLoading: boolean, isError: boolean) {
-    if (isLoading) {
-      return "Loading...";
-    }
-    if (isError) {
-      return "Error loading logs";
-    }
-    if (!data) {
-      return "File is empty";
-    }
-    return data
-  }
-
   return (
-    <Tabs defaultValue="stdout">
-      <TabsList>
-        <TabsTrigger value="stdout">stdout</TabsTrigger>
-        <TabsTrigger value="stderr">stderr</TabsTrigger>
-      </TabsList>
-      <TabsContent value="stdout" className="h-72">
-        <Textarea
-          className="text-md h-full w-full resize-none bg-muted shadow-lg"
-          value={checkData(stdout.data, stdout.isLoading, stdout.isError)}
-          readOnly={true}
-        />
-      </TabsContent>
-      <TabsContent value="stderr" className="h-72">
-        <Textarea
-          className="text-md h-full w-full resize-none bg-muted shadow-lg"
-          value={checkData(stderr.data, stderr.isLoading, stderr.isError)}
-          readOnly={true}
-        />
-      </TabsContent>
-    </Tabs>
+    <>
+      <Tabs defaultValue="stdout" className="relative">
+        <TabsList>
+          <TabsTrigger value="stdout">stdout</TabsTrigger>
+          <TabsTrigger value="stderr">stderr</TabsTrigger>
+        </TabsList>
+        <TabsContent value="stdout" className="h-72">
+          <Textarea
+            className="text-md h-full w-full resize-none bg-muted shadow-lg"
+            value={stdout.data ?? stdout.error?.message}
+            readOnly={true}
+          />
+        </TabsContent>
+        <TabsContent value="stderr" className="h-72">
+          <Textarea
+            className="text-md h-full w-full resize-none bg-muted shadow-lg"
+            value={stderr.data ?? stderr.error?.message}
+            readOnly={true}
+          />
+        </TabsContent>
+      </Tabs>
+    </>
   );
 }
