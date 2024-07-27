63bad02d7148721c66a3c5979e2cac604b26eb7a
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-07-27 14:26:00 -0300

    form schemas for accepted extensions of image and annotation files.

---
+++ src/lib/schemas/input-files.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const imagePathSchema = z
+  .string()
+  .min(2, { message: "Must be a valid image name!" })
+  .regex(/^.*\.(tif|tiff|TIFF|hdf5|h5|raw|b)$/, {
+    message: "Must be a valid image extension!",
+  });
+
+export const annotationPathSchema = z
+  .string()
+  .min(2, { message: "Must be a valid annotation name!" })
+  .regex(/^.*\.(pkl)$/, {
+    message: "Must be a valid annotation extension!",
+  });
