fdf469135c57adc7607bada1b660f29b61d07e09
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-07-29 10:35:49 -0300

    use the validImageExtensions const for validating the schema instead of rewriting its validation as a regex pattern

--- src/lib/schemas/form-input-paths.ts
+++ src/lib/schemas/form-input-paths.ts
@@ -1,26 +1,41 @@
 import { z } from "zod";
 
-export const validImageExtensions = ["tif", "tiff", "TIFF", "hdf5", "h5", "raw", "b"] as const;
+export const validImageExtensions = [
+  "tif",
+  "tiff",
+  "TIFF",
+  "hdf5",
+  "h5",
+  "raw",
+  "b",
+] as const;
 export const validAnnotationExtensions = ["pkl"] as const;
 
 export const imagePathSchema = z
   .string()
   .min(2, { message: "Must be a valid image name!" })
-  .regex(/^.*\.(tif|tiff|TIFF|hdf5|h5|raw|b)$/, {
-    message: "Must be a valid image extension!",
-  });
+  .refine(
+    (value) => {
+      const extension = value.split(".").pop();
+      return validImageExtensions.includes(
+        extension as (typeof validImageExtensions)[number],
+      );
+    },
+    {
+      message: `Image file must be one of the following types: ${validImageExtensions.join(', ')}`,
+    },
+  );
 
 export const annotationPathSchema = z
   .string()
   .min(2, { message: "Must be a valid annotation name!" })
-  .regex(/^.*\.(pkl)$/, {
-    message: "Must be a valid annotation extension!",
+  .endsWith(validAnnotationExtensions[0], {
+    message: "Annotation file must be a .pkl file!",
   });
 
 export const outputDirSchema = z
   .string()
   .min(2, { message: "Must be a valid workspace directory!" })
-  .regex(/\/$/, {
+  .endsWith("/", {
     message: "Must be a valid workspace directory!",
   });
-
